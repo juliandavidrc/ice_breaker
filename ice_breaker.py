81171684a9dbe84adba26b7cb6ca9b69765f3472
Julian Romero <juliandavirc@gmail.com>
2023-09-11 22:32:52 -0500

    change Json return

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,8 +2,11 @@ import os
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+
+
 from third_parties.gist_profile import gist_response_lkprof
 
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 
 # information = """Elon Reeve Musk (Pretoria, 28 de junio de 1971), conocido como Elon Musk, es un empresario, inversor y magnate sudafricano que también posee las nacionalidades canadiense y estadounidense. Es el fundador, consejero delegado e ingeniero jefe de SpaceX; inversor ángel, director general y arquitecto de productos de Tesla, Inc.; fundador de The Boring Company; cofundador de Neuralink y OpenAI, aunque ya no tiene más participación en esta última por desacuerdos en el rumbo de la empresa, además de ser el director de tecnología de X Corp..4​ Con un patrimonio neto estimado en unos 207 mil millones de dólares en junio de 2023,5​ Musk es la persona más rica del mundo según el índice de multimillonarios de Bloomberg y la lista de multimillonarios en tiempo real de Forbes.6​7​
@@ -11,9 +14,11 @@ from third_parties.linkedin import scrape_linkedin_profile
 # En 2002, Musk fundó SpaceX, fabricante aeroespacial y empresa de servicios de transporte espacial, de la que es CEO e ingeniero jefe. En 2003, se unió al fabricante de vehículos eléctricos Tesla Motors, Inc. (ahora Tesla, Inc.) como presidente y arquitecto de productos, convirtiéndose en su consejero delegado en 2008. En 2006, ayudó a crear SolarCity, una empresa de servicios de energía solar que posteriormente fue adquirida por Tesla y se convirtió en Tesla Energy. En 2015, cofundó OpenAI, una empresa de investigación sin ánimo de lucro que promueve la inteligencia artificial amigable. En 2016, cofundó Neuralink, una empresa de neurotecnología centrada en el desarrollo de interfaces cerebro-ordenador, y fundó The Boring Company, una empresa de construcción de túneles. También acordó la compra de la importante red social estadounidense Twitter en 2022 por 44 000 millones de dólares. Musk también ha propuesto el hyperloop. En noviembre de 2021, el director general de Tesla fue la primera persona de la historia en acumular una fortuna de 300 000 millones de dólares.8​"""
 
 if __name__ == "__main__":
-    print("Hello LangCHain!")
+    print("Hello LangChain!")
     # print(os.environ['OPENAI_API_KEY'])
     print(os.environ["PROXYCURL_API_KEY"])
+
+    linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco")
 
     summary_template = """
         Given the information {information} about a person from I want yuo create:
@@ -29,12 +34,14 @@ if __name__ == "__main__":
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = gist_response_lkprof()
-    # print(linkedin_data)
-
-    #  linkedin_data = scrape_linkedin_profile(
-    #     linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
-    # )
-    # print(linkedin_data.json())
+    # ** Take linkedin profile from githubuserConnect JSON **
+    #linkedin_data = gist_response_lkprof()
+    # ** ************************************************* **
+    
+    linkedin_data = scrape_linkedin_profile(
+        #linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
+        linkedin_profile_url=linkedin_profile_url
+    )
+    #print(linkedin_data.json())
 
     print(chain.run(information=linkedin_data))
